e838baa86a3fbe0cd94b4e5247cb7919fe936419
gsramos1991 <gabrielsr1991@outlook.com>
2025-06-24 20:08:36 -0300

    add hh:mm:ss

--- Program.cs
+++ Program.cs
@@ -3,5 +3,5 @@
 while(i < 100000)
 {
     i++;
-    Console.WriteLine(DateTime.Now.ToString("dd/MM/yyyy") +" " + i);
+    Console.WriteLine(DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss") +" " + i);
 }
