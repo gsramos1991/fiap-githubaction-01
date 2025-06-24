f45cc1c44f64a37c20b037d86b2fddbd3b3beae0
gsramos1991 <gabrielsr1991@outlook.com>
2025-06-24 20:19:50 -0300

    add nova mensagem

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿int i = 0;
 
-while(i < 100000)
+while(i < 10)
 {
     i++;
+    Console.WriteLine(DateTime.Now.ToString("Qual eh a Hora?") );
     Console.WriteLine(DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss") +" " + i);
 }
